0293adac7a0e17fa7e4a1d1c5985f3159c677252
jitong <2406677340@qq.com>
2014-11-17 14:27:02 +0000

    整理
    git-svn-id: http://localhost:8080/svn/jitongr@22 0e54bc44-067d-d140-a5ad-a89cec78df39

---
+++ pic.php
@@ -0,0 +1,11 @@
+<?php
+require_once '../init.php';
+echo "building...";
+$imgid = isset ($_GET['img']) ? intval ($_GET['img']) : '';
+echo $imgid;
+
+
+
+
+
+?>
---
+++ view/msg.php
@@ -0,0 +1,6 @@
+<?php if(!defined('EMLOG_ROOT')) {exit('error!');}?>
+
+<div id="m">
+<div><?php echo $msg;?></div>
+<div><a href="<?php echo $url; ?>">返回</a></div>
+</div>
